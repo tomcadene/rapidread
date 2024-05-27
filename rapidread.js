5c306d7bc542b18468e26429e524fa0799d16655
tomcadene <165381734+tomcadene@users.noreply.github.com>
2024-05-27 16:43:56 +0200

    Update rapidread.js

--- rapidread.js
+++ rapidread.js
@@ -45,7 +45,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    
     // Function to toggle bionic reading mode
     function toggleBionicReading() {
         const elements = document.querySelectorAll(TEXT_CONTAINERS.join(', '));
